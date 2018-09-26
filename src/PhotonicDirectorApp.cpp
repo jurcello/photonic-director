8bd4c0172a0f4327812c0a3520e3aa5b98975467
Jur de Vries <info@jurdevries.nl>
2018-09-26 14:41:44 +0200

    Auto channel generation done.

--- src/PhotonicDirectorApp.cpp
+++ src/PhotonicDirectorApp.cpp
@@ -99,12 +99,15 @@ protected:
 
     // Channel stuff.
     vector<InputChannelRef> mChannels;
+    vector<std::string> mIncomingOscAdresses;
+    double mLastChannelGeneration;
     
     // Effects.
     vector<EffectRef> mEffects;
     double mLastUpdate;
     
     void oscReceive(const osc::Message &message);
+    void updateChannelsFromOsc();
     void handleLightOscSending();
     void handleCalibrationSending();
     // Gui stuff.
@@ -135,6 +138,7 @@ PhotonicDirectorApp::PhotonicDirectorApp()
   mShowVisualizer(true),
   mShowObjects(false),
   mLastOscAddress(""),
+  mLastChannelGeneration(0.0),
   mUnityAddress("192.168.1.8"),
   mUnityPort(8089)
 {
@@ -150,58 +154,7 @@ PhotonicDirectorApp::PhotonicDirectorApp()
 }
 
 void PhotonicDirectorApp::setTheme(ImGui::Options &options) {
-//    options.childWindowRounding(3.f);
-    options.grabRounding(0.f);
-    options.windowRounding(0.f);
-    options.scrollbarRounding(3.f);
-    options.frameRounding(3.f);
-    options.windowTitleAlign(ImVec2(0.5f,0.5f));
-    
-    
-    options.color(ImGuiCol_Text,                  ImVec4(0.73f, 0.73f, 0.73f, 1.00f));
-    options.color(ImGuiCol_TextDisabled,          ImVec4(0.50f, 0.50f, 0.50f, 1.00f));
-    options.color(ImGuiCol_WindowBg,              ImVec4(0.26f, 0.26f, 0.26f, 0.95f));
-    options.color(ImGuiCol_ChildWindowBg,         ImVec4(0.28f, 0.28f, 0.28f, 1.00f));
-    options.color(ImGuiCol_PopupBg,               ImVec4(0.26f, 0.26f, 0.26f, 1.00f));
-    options.color(ImGuiCol_Border,                ImVec4(0.26f, 0.26f, 0.26f, 1.00f));
-    options.color(ImGuiCol_BorderShadow,          ImVec4(0.26f, 0.26f, 0.26f, 1.00f));
-    options.color(ImGuiCol_FrameBg,               ImVec4(0.16f, 0.16f, 0.16f, 1.00f));
-    options.color(ImGuiCol_FrameBgHovered,        ImVec4(0.16f, 0.16f, 0.16f, 1.00f));
-    options.color(ImGuiCol_FrameBgActive,         ImVec4(0.16f, 0.16f, 0.16f, 1.00f));
-    options.color(ImGuiCol_TitleBg,               ImVec4(0.36f, 0.36f, 0.36f, 1.00f));
-    options.color(ImGuiCol_TitleBgCollapsed,      ImVec4(0.36f, 0.36f, 0.36f, 1.00f));
-    options.color(ImGuiCol_TitleBgActive,         ImVec4(0.36f, 0.36f, 0.36f, 1.00f));
-    options.color(ImGuiCol_MenuBarBg,             ImVec4(0.26f, 0.26f, 0.26f, 1.00f));
-    options.color(ImGuiCol_ScrollbarBg,           ImVec4(0.21f, 0.21f, 0.21f, 1.00f));
-    options.color(ImGuiCol_ScrollbarGrab,         ImVec4(0.36f, 0.36f, 0.36f, 1.00f));
-    options.color(ImGuiCol_ScrollbarGrabHovered,  ImVec4(0.36f, 0.36f, 0.36f, 1.00f));
-    options.color(ImGuiCol_ScrollbarGrabActive,   ImVec4(0.36f, 0.36f, 0.36f, 1.00f));
-//    options.color(ImGuiCol_ComboBg,               ImVec4(0.32f, 0.32f, 0.32f, 1.00f));
-    options.color(ImGuiCol_CheckMark,             ImVec4(0.78f, 0.78f, 0.78f, 1.00f));
-    options.color(ImGuiCol_SliderGrab,            ImVec4(0.74f, 0.74f, 0.74f, 1.00f));
-    options.color(ImGuiCol_SliderGrabActive,      ImVec4(0.74f, 0.74f, 0.74f, 1.00f));
-    options.color(ImGuiCol_Button,                ImVec4(0.36f, 0.36f, 0.36f, 1.00f));
-    options.color(ImGuiCol_ButtonHovered,         ImVec4(0.43f, 0.43f, 0.43f, 1.00f));
-    options.color(ImGuiCol_ButtonActive,          ImVec4(0.11f, 0.11f, 0.11f, 1.00f));
-    options.color(ImGuiCol_Header,                ImVec4(0.36f, 0.36f, 0.36f, 1.00f));
-    options.color(ImGuiCol_HeaderHovered,         ImVec4(0.36f, 0.36f, 0.36f, 1.00f));
-    options.color(ImGuiCol_HeaderActive,          ImVec4(0.36f, 0.36f, 0.36f, 1.00f));
-    options.color(ImGuiCol_Column,                ImVec4(0.39f, 0.39f, 0.39f, 1.00f));
-    options.color(ImGuiCol_ColumnHovered,         ImVec4(0.26f, 0.59f, 0.98f, 1.00f));
-    options.color(ImGuiCol_ColumnActive,          ImVec4(0.26f, 0.59f, 0.98f, 1.00f));
-    options.color(ImGuiCol_ResizeGrip,            ImVec4(0.36f, 0.36f, 0.36f, 1.00f));
-    options.color(ImGuiCol_ResizeGripHovered,     ImVec4(0.26f, 0.59f, 0.98f, 1.00f));
-    options.color(ImGuiCol_ResizeGripActive,      ImVec4(0.26f, 0.59f, 0.98f, 1.00f));
-    options.color(ImGuiCol_CloseButton,           ImVec4(0.59f, 0.59f, 0.59f, 1.00f));
-    options.color(ImGuiCol_CloseButtonHovered,    ImVec4(0.98f, 0.39f, 0.36f, 1.00f));
-    options.color(ImGuiCol_CloseButtonActive,     ImVec4(0.98f, 0.39f, 0.36f, 1.00f));
-    options.color(ImGuiCol_PlotLines,             ImVec4(0.39f, 0.39f, 0.39f, 1.00f));
-    options.color(ImGuiCol_PlotLinesHovered,      ImVec4(1.00f, 0.43f, 0.35f, 1.00f));
-    options.color(ImGuiCol_PlotHistogram,         ImVec4(0.90f, 0.70f, 0.00f, 1.00f));
-    options.color(ImGuiCol_PlotHistogramHovered,  ImVec4(1.00f, 0.60f, 0.00f, 1.00f));
-    options.color(ImGuiCol_TextSelectedBg,        ImVec4(0.32f, 0.52f, 0.65f, 1.00f));
-    options.color(ImGuiCol_ModalWindowDarkening,  ImVec4(0.20f, 0.20f, 0.20f, 0.50f));
-    options.color(ImGuiCol_TextSelectedBg,        ImVec4(0.00f, 0.20f, 1.00f, 1.00f));
+    options = options.darkTheme();
 }
 
 void PhotonicDirectorApp::setup()
@@ -289,6 +242,9 @@ void PhotonicDirectorApp::setupOsc(int receivePort, int sendPort)
 void PhotonicDirectorApp::oscReceive(const osc::Message &message)
 {
     mLastOscAddress = message.getAddress();
+    if (std::find(mIncomingOscAdresses.begin(), mIncomingOscAdresses.end(), mLastOscAddress) == mIncomingOscAdresses.end()) {
+        mIncomingOscAdresses.push_back(mLastOscAddress);
+    }
     if (mChannels.size() > 0) {
         for (InputChannelRef channel : mChannels) {
             if (message.getAddress() == channel->getAddress()) {
@@ -326,7 +282,24 @@ void PhotonicDirectorApp::oscReceive(const osc::Message &message)
     for (auto effect : mEffects) {
         effect->listenToOsc(message);
     }
+    updateChannelsFromOsc();
+}
 
+void PhotonicDirectorApp::updateChannelsFromOsc() {
+    if (mLastUpdate - mLastChannelGeneration > 2.0){
+        for (auto address: mIncomingOscAdresses) {
+            bool found = false;
+            for (auto channel: mChannels) {
+                if (channel->getAddress() == address) {
+                    found = true;
+                }
+            }
+            if (!found) {
+                mChannels.push_back(InputChannel::create(address, address));
+            }
+        }
+        mLastChannelGeneration = mLastUpdate;
+    }
 }
 
 void PhotonicDirectorApp::mouseDown( MouseEvent event )
